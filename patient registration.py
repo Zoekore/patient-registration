0339252f3857f4a464a799fe77493ae183738919
samuel <samuelolugua@gmail.com>
2024-06-20 13:09:12 +0100

    added a new code

--- patient registration.py
+++ patient registration.py
@@ -2,6 +2,8 @@ name = input("enter:")
 age = int(input("enter:"))
 if age > 12:
     print(name + "is a new patient")
-else:
+elif age == 18 :
     print(name + "is an existing patient")
+else :
+    print(name + "is already a patient")
 
